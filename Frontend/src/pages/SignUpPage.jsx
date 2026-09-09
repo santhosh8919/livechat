7ff5abe2847c20c6196ed39c2aa7115da068287d
SANTHOSH MUDAVATH <157015703+santhosh8919@users.noreply.github.com>
2026-09-09 20:47:56 +0530

    Update placeholder text in SignUpPage

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -59,7 +59,7 @@ const SignUpPage = () => {
                     </label>
                     <input
                       type="text"
-                      placeholder="Ramsingh"
+                      placeholder="santhosh"
                       className="input input-bordered w-full"
                       value={signupData.fullName}
                       onChange={(e) => setSignupData({ ...signupData, fullName: e.target.value })}
@@ -73,7 +73,7 @@ const SignUpPage = () => {
                     </label>
                     <input
                       type="email"
-                      placeholder="ramu@gmail.com"
+                      placeholder="santhosh@gmail.com"
                       className="input input-bordered w-full"
                       value={signupData.email}
                       onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
