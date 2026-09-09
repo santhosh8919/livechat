f8db5201f6e037f34d0f89c4eb8ae615606c0054
SANTHOSH MUDAVATH <157015703+santhosh8919@users.noreply.github.com>
2026-09-09 20:50:59 +0530

    Update SignUpPage.jsx

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -59,7 +59,7 @@ const SignUpPage = () => {
                     </label>
                     <input
                       type="text"
-                      placeholder="santhosh"
+                      placeholder="Ramsingh"
                       className="input input-bordered w-full"
                       value={signupData.fullName}
                       onChange={(e) => setSignupData({ ...signupData, fullName: e.target.value })}
@@ -73,7 +73,7 @@ const SignUpPage = () => {
                     </label>
                     <input
                       type="email"
-                      placeholder="santhosh@gmail.com"
+                      placeholder="ramu@gmail.com"
                       className="input input-bordered w-full"
                       value={signupData.email}
                       onChange={(e) => setSignupData({ ...signupData, email: e.target.value })}
